5917aa0a0a28422c48ae003d0443d2504729493a
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-04-19 22:00:10 -0600

    Add broken calculation logic

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(a,b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a,b) {
@@ -13,10 +13,6 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b;
 }
-
-let num1;
-let num2;
-let operator;
 
 function operate(num1, num2, operator) {
     if (operator === '+') {
@@ -43,10 +39,14 @@ function populate(button) {
     else if (typeof button === 'number') {
         display.textContent += button;
     }
+    /*
     else {
-        console.log(store(display.textContent));
+        let num1 = store(display.textContent.replace(/\D/g, ""));
         display.textContent = button;
+        return num1;
+    
     }
+    */
 }
 /*
 I need the algorithm to store the number and store the operator 
@@ -62,14 +62,44 @@ operator.
 const digits = document.querySelectorAll('.digits > button');
 digits.forEach(function(digit) {
     digit.addEventListener('click', function() {
-        populate(parseInt(digit.textContent))
+        populate(Number(digit.textContent))
     })
 })
 
 const operations = document.querySelectorAll('.operations > button');
+let firstClick = true;
+let op;
+let num1;
+let num2 = false;
+let outcome;
 operations.forEach(function(operation) {
     operation.addEventListener('click', function() {
-        populate(operation.textContent);
+        if (firstClick) {
+            num1 = display.textContent;
+            op = operation.textContent;
+            display.textContent = "";
+            firstClick = false;
+        }
+        else {
+            if (!num2) {
+                num2 = display.textContent;
+                outcome = operate(num1, num2, op);
+                console.log(outcome);
+                num1 = num2;
+                num2 = outcome;
+                display.textContent = "";
+                op = operation.textContent;
+            }
+            else {
+                outcome = operate(num1, num2, op);
+                num2 = display.textContent;
+                console.log(outcome);
+                num1 = num2;
+                num2 = outcome;
+                display.textContent = "";
+                op = operation.textContent;
+            }
+        }
     })
 })
 
